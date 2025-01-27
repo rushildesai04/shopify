439541a383cfb06e4632fe840257a77fd8f5594a
Rushil Desai <rushil.desai.04@gmail.com>
2025-01-27 06:41:44 -0800

    Built data structures for the respective products and orders as well as implementing the constraints for each order

--- merchants.py
+++ merchants.py
@@ -23,6 +23,33 @@ class order:
         self.product = product
         self.quantity = quantity
 
+    def output_within_country(self):
+        warehouses = self.product.warehouses
+
+        for wh in warehouses:
+            if self.buyer.country != wh.country:
+                warehouses.pop(wh)
+        
+        return warehouses
+    
+    def output_euc_dist(self):
+        warehouses = self.product.warehouses
+        min_dist = 0
+        min_wh = None
+        for wh in warehouses:
+            euc_dist = (((self.buyer.coordinates[0] - wh.coordinates[0]) ** 2) + ((self.buyer.coordinates[1] - wh.coordinates[1]) ** 2)) ** 0.5
+            if euc_dist < min_dist:
+                min_dist = euc_dist
+                min_wh = wh
+        return min_wh
+
+    def output_availability(self):
+        warehouses = self.product.warehouses
+        for wh in warehouses:
+            if self.quantity > wh.stock:
+                warehouses.pop(wh)
+        return warehouses
+
 class data:
     def __init__(self):
         self.products = []
